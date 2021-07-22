5f8550eb29da0f1b68ce17fc010308a21b25f17b
felipe <@feilpeEAC>
2021-07-22 00:28:05 -0300

    relogio analogico

--- script.js
+++ script.js
@@ -11,20 +11,25 @@ function updateClock(){
     let second = now.getSeconds();
 
     digitalelement.innerHTML =`${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;  //innerHTML = adciona um texto : : :
+   
+    let sDeg = ((360 / 60) * second) - 90;         //Divide o raio de um circulo pelo total de segundos , assim iremos descobre quantos graus possui cada segundo
+    let mDeg = ((360 / 60) * minute) - 90; 
+    let hDeg = ((360 / 12) * hour)   - 90;
+    
+    //sElement.style.transform = 'rotate(-90deg)'; // insere na varial sElement  um estilo css , style.tranform , o rotate informa qual a posiçao do ponteiro
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 
 }
 
 function fixZero(time){
-  /*  if(time < 10 ){
+    if(time < 10 ){
         return '0' + time;
     }else{
         return time;
-    }
-    */
-
-     return time <10 ? `0${time}` : time;         // forma alternativa
-
+    }   
+    // return time <10 ? `0${time}` : time;         // forma alternativa
 }
-
-
 setInterval(updateClock, 1000);                 // cria um intervalo para contagem de tempo
+updateClock();
